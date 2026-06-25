c1470a9de299f8c0c8f2ee848d7b736a6765e46a
Bigchungusfloppa <vidyajagtap89@gmail.com>
2026-06-26 03:23:19 +0530

    Initial commit - Todo app

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const heading = document.querySelector("h1");
+const input = document.querySelector("#todoInput");
+const button = document.getElementById("addButton");
+const list = document.getElementById("todoList");
+
+console.log(heading);
+console.log(input);
+console.log(button);
+console.log(list);
