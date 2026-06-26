d6f942c390cbfc063f8043f03c7d8d55cc938000
Bigchungusfloppa <vidyajagtap89@gmail.com>
2026-06-27 04:13:23 +0530

    Added eventListener and input values

--- public/script.js
+++ public/script.js
@@ -3,6 +3,21 @@ const input = document.querySelector("#todoInput");
 const button = document.getElementById("addButton");
 const list = document.getElementById("todoList");
 
+button.addEventListener("click", () => {
+
+    const text = input.value.trim();
+
+    if (text == "") return;
+
+    const li = document.createElement("li");
+    li.textContent = text;
+    list.appendChild(li);
+
+    input.value = "";
+
+
+});
+
 console.log(heading);
 console.log(input);
 console.log(button);
